5e15f0cc246a1297d00ace1084e4d86b5de5a65f
sudhakartajalfahidi <sudhakar.tajalfahidi@gmail.com>
2024-10-17 14:50:49 +0400

    sync bank otp added

--- SmsVerification.js
+++ SmsVerification.js
@@ -1,41 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TextInput, Alert, PermissionsAndroid, Platform, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, PermissionsAndroid, Platform, Alert, StyleSheet, TouchableOpacity } from 'react-native';
 import SmsAndroid from 'react-native-get-sms-android';
 import axios from 'axios';
 
 const SmsVerification = () => {
   const [otpMessages, setOtpMessages] = useState([]);
-  const [isPolling, setIsPolling] = useState(true);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [token, setToken] = useState('');
 
   useEffect(() => {
-    if (isLoggedIn) {
-      requestSmsPermission();
-    }
-
-    let intervalId;
-    if (isPolling && isLoggedIn) {
-      intervalId = setInterval(() => {
-        fetchSmsMessages();
-      }, 30000); // Poll every 30 seconds
-    }
-
-    return () => clearInterval(intervalId);
-  }, [isPolling, isLoggedIn]);
-
-  const handleLogin = async () => {
-    try {
-      const response = await axios.post('http://172.16.20.208:6000/login', { username, password });
-      setToken(response.data.token);
-      setIsLoggedIn(true);
-    } catch (error) {
-      console.error('Login error:', error);
-      Alert.alert('Login Failed', 'Invalid credentials');
-    }
-  };
+    requestSmsPermission();
+    fetchSmsMessages(); 
+  }, []);
 
   const requestSmsPermission = async () => {
     if (Platform.OS === 'android') {
@@ -61,7 +35,6 @@ const SmsVerification = () => {
   };
 
   const fetchSmsMessages = () => {
-    console.log('Fetching SMS messages...');
     SmsAndroid.list(
       JSON.stringify({
         box: 'inbox',
@@ -74,17 +47,17 @@ const SmsVerification = () => {
       (count, smsList) => {
         try {
           const messages = JSON.parse(smsList);
-          // Sort messages by date in descending order to get the latest one
-          messages.sort((a, b) => new Date(b.date) - new Date(a.date));
-          
-          const latestOtp = messages
-            .map((sms) => parseOneTimeCode(sms.body))
-            .find((otp) => otp !== '');
+          const otpData = messages
+            .map((sms) => {
+              const otp = parseOneTimeCode(sms.body);
+              return otp ? { otp, address: sms.address } : null;
+            })
+            .filter(Boolean);
+          console.log("🚀 ~ fetchSmsMessages ~ otpData:", otpData)
 
-          if (latestOtp) {
-            setOtpMessages([latestOtp]); // Store only the latest OTP
-            setIsPolling(false);
-            sendOtpsToServer([latestOtp]);
+          if (otpData.length > 0) {
+            setOtpMessages(otpData);
+            sendOtpsToServer(otpData);
           }
         } catch (error) {
           console.error('Error parsing SMS list:', error);
@@ -95,16 +68,14 @@ const SmsVerification = () => {
   };
 
   const parseOneTimeCode = (message) => {
-    const regex = /\b\d{4,10}\b/; // Adjust regex if needed
+    const regex = /\b\d{4,6}\b/; 
     const match = message.match(regex);
     return match ? match[0] : '';
   };
 
-  const sendOtpsToServer = async (otps) => {
+  const sendOtpsToServer = async (otpData) => {
     try {
-      await axios.post('http://172.16.20.208:6000/receive-otp', { otp: otps }, {
-        headers: { Authorization: `Bearer ${token}` }
-      });
+      await axios.post('http://localhost:5000/receive-otp', { otpData });
       console.log('OTPs sent to the server successfully');
     } catch (error) {
       console.error('Failed to send OTPs to server:', error);
@@ -113,46 +84,23 @@ const SmsVerification = () => {
 
   return (
     <View style={styles.container}>
-      {!isLoggedIn ? (
-        <View style={styles.loginContainer}>
-          <Text style={styles.header}>Login</Text>
-          <TextInput
-            style={styles.input}
-            placeholder="Username"
-            value={username}
-            onChangeText={setUsername}
-          />
-          <TextInput
-            style={styles.input}
-            placeholder="Password"
-            value={password}
-            onChangeText={setPassword}
-            secureTextEntry
-          />
-          <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
-            <Text style={styles.loginButtonText}>Login</Text>
-          </TouchableOpacity>
-        </View>
-      ) : (
-        <>
-          <Text style={styles.header}>SMS OTP Extraction</Text>
-          <TouchableOpacity style={styles.fetchButton} onPress={fetchSmsMessages}>
-            <Text style={styles.buttonText}>Fetch SMS from Inbox</Text>
-          </TouchableOpacity>
+      <Text style={styles.header}>SMS OTP Extraction</Text>
+      <TouchableOpacity style={styles.fetchButton} onPress={fetchSmsMessages}>
+        <Text style={styles.buttonText}>Fetch SMS from Inbox</Text>
+      </TouchableOpacity>
 
-          <View style={styles.messageContainer}>
-            {otpMessages.length > 0 ? (
-              otpMessages.map((otp, index) => (
-                <View key={index} style={styles.otpCard}>
-                  <Text style={styles.otpText}>{otp}</Text>
-                </View>
-              ))
-            ) : (
-              <Text style={styles.noMessages}>No OTP messages found.</Text>
-            )}
-          </View>
-        </>
-      )}
+      <View style={styles.messageContainer}>
+        {otpMessages.length > 0 ? (
+          otpMessages.map((otpData, index) => (
+            <View key={index} style={styles.otpCard}>
+              <Text style={styles.otpText}>OTP: {otpData.otp}</Text>
+              <Text style={styles.otpText}>From: {otpData.address}</Text>
+            </View>
+          ))
+        ) : (
+          <Text style={styles.noMessages}>No OTP messages found.</Text>
+        )}
+      </View>
     </View>
   );
 };
@@ -170,36 +118,6 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: '#4A4A4A',
     marginBottom: 30,
-  },
-  input: {
-    width: '100%',
-    padding: 12,
-    marginBottom: 20,
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 10,
-    backgroundColor: '#fff',
-    fontSize: 16,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.1,
-    shadowRadius: 4,
-  },
-  loginContainer: {
-    width: '100%',
-    alignItems: 'center',
-  },
-  loginButton: {
-    backgroundColor: '#6200EE',
-    paddingVertical: 15,
-    paddingHorizontal: 40,
-    borderRadius: 10,
-    marginTop: 20,
-  },
-  loginButtonText: {
-    color: '#fff',
-    fontSize: 18,
-    fontWeight: 'bold',
   },
   fetchButton: {
     backgroundColor: '#6200EE',
